ff4803bb3fa9700c09041e4078830c82174dd715
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-09-20 18:09:03 +0200

    ff

--- src/services/TheMoviesApi.js
+++ src/services/TheMoviesApi.js
@@ -56,7 +56,7 @@ export const fetchMovieReviews = async (movieId) => {
 
 export const fetchMovieQuery = async (query) => {
     try {
-        const response = await axios.get(`${BASE_URL}/search/movie`, ...options);
+        const response = await axios.get(`${BASE_URL}/search/movie`, options);
         return response.data.results; 
     } catch (error) {
         console.error('Error fetching movies by query:', error);
