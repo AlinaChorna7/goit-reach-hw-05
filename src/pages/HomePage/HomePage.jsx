5c6fe79c2f977bb2ee53d0809d9e8babf670d1af
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-09-20 15:40:15 +0200

    d

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react"
 import { fetchTrendingMovies } from "../../services/TheMoviesApi"
 import { Link } from "react-router-dom";
-import styles from './homepage.module.css';
+import styles from './HomePage.module.css';
 
 
 export default function HomePage(){
